814d87ac109aa012784aba9857a50383556fc912
jay solanki <jaysolanki46@gmail.com>
2019-05-29 18:19:49 +1200

    Category and Product category designed

--- app/src/main/java/com/example/jayso/shopnsaveadmin/ProductCategoryFragment.java
+++ app/src/main/java/com/example/jayso/shopnsaveadmin/ProductCategoryFragment.java
@@ -6,6 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.jayso.shopnsaveadmin.com.example.jayso.shopnsaveadmin.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +23,7 @@ import android.view.ViewGroup;
  */
 public class ProductCategoryFragment extends Fragment {
 
+    Spinner spinner;
 
     public ProductCategoryFragment() {
         // Required empty public constructor
@@ -22,8 +33,43 @@ public class ProductCategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Grap category from database
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1, "Category 1"));
+        categories.add(new Category(2, "Category 2"));
+        categories.add(new Category(3, "Category 3"));
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_category, container, false);
+        spinner = (Spinner) view.findViewById(R.id.spinner_categories);
+        ArrayAdapter < Category > adapter =
+                new ArrayAdapter < Category > (getActivity(), android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Category category = (Category) parent.getSelectedItem();
+                //display(category);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return view;
     }
 
+//    public void getSelectedCategory(View view) {
+//        Category category = (Category) spinner.getSelectedItem();
+//    }
+//
+//    private void display(Category category) {
+//        String name = category.getCat_name();
+//        Toast.makeText(getActivity(), name, Toast.LENGTH_LONG).show();
+//    }
 }
